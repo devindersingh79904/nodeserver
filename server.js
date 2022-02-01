2731c40245b7abc38bf2883ebf43cac1fa36849a
devinder singh <devindersingh79904@gmail.com>
2022-02-02 02:01:03 +0530

    config check

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT} .`))
