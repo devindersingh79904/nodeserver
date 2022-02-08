0769daad3bf64ee29fc06c310869aa03f5d63267
devinder singh <devindersingh79904@gmail.com>
2022-02-08 13:57:04 +0530

    error constant added

---
+++ Utils/ErrorConstant.js
@@ -0,0 +1,9 @@
+const DUPLICATE_KEY_ERROR = 11000
+const CAST_ERROR = 'CastError'
+const VALIDATION_ERROR = 'ValidationError'
+
+module.exports = {
+    DUPLICATE_KEY_ERROR,
+    CAST_ERROR,
+    VALIDATION_ERROR
+}
--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,5 +1,6 @@
+const { DUPLICATE_KEY_ERROR, CAST_ERROR, VALIDATION_ERROR } = require("../Utils/ErrorConstant");
 const ErrorResponse = require("../Utils/ErrorResponse");
-const { SERVER_ERROR, NOT_FOUND } = require("../Utils/httpConst");
+const { SERVER_ERROR, NOT_FOUND, BAD_REQUEST } = require("../Utils/httpConst");
 
 const errorHandler = (err,req,res,next) => {
     let error = {...err}
@@ -8,16 +9,16 @@ const errorHandler = (err,req,res,next) => {
     console.log(err)
     console.log(err.name)
 
-    if(err.name==='CastError'){
-        const message = `Bootcamp not found with id of ${err.value}`
+    if(err.name === CAST_ERROR){
+        const message = `Bootcamp not found with id ${err.value}`
         error = new ErrorResponse(message,NOT_FOUND)
     }
-    else if(err.code == 11000){
-        const message = `Duplicate field value added`
-        error = new ErrorResponse(message,NOT_FOUND)
+    else if(err.code === DUPLICATE_KEY_ERROR){
+        const message = `${err.message}`
+        error = new ErrorResponse(message,BAD_REQUEST)
     }
-    else if(err.name === 'ValidationError'){
-        let message = "ValidationError : " + Object.values(err.errors).map(error => error.message)
+    else if( err.name === VALIDATION_ERROR){
+        const message = Object.values(err.errors).map(err=> err.message)
         error = new ErrorResponse(message,NOT_FOUND)
     }
 
