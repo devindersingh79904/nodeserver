c1bf6fde1f0ef0255a8f95685fce3c7b4da8a4fc
devinder singh <devindersingh79904@gmail.com>
2022-02-09 15:00:41 +0530

    bootcamps find by radius

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("../middleware/asyncHandler")
 const Bootcamp = require("../models/Bootcamp")
 const ErrorResponse = require("../Utils/ErrorResponse")
+const geocoder = require("../Utils/geocoder")
 const { CREATED, SUCCESS, NOT_FOUND } = require("../Utils/httpConst")
 
 
@@ -56,4 +57,40 @@ exports.deleteBootcamp = asyncHandler(async(req,res,next) => {
     }
     res.status(SUCCESS).json({success:true,msg:'bootcamp deleted succesfully'})
 })
+
+
+exports.getBootcampsByRadius = asyncHandler(async(req,res,next) => {
+    
+    const { zipcode, distance } = req.params;
+    
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    let lat,lng
+    loc.forEach(location => {
+        if(location.zipcode === zipcode)
+        {
+            lat = location.latitude;
+            lng = location.longitude;
+        }
+        
+    })
+    
+
+    // Calc radius using radians
+    // Divide dist by radius of Earth
+    // Earth Radius = 3,963 mi / 6,378 km
+    const radius = distance / 3963.2;
+  
+    const bootcamps = await Bootcamp.find({
+      location: { $geoWithin: { $centerSphere: [ [ lng,lat ], radius ] } }
+    });
+  
+    res.status(200).json({
+      success: true,
+      count: bootcamps.length,
+      data: bootcamps
+    });
+
+})
+
 
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { getBootcamps, getBootcamp, postBootcamp, putBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
+const { getBootcamps, getBootcamp, postBootcamp, putBootcamp, deleteBootcamp, getBootcampsByRadius } = require('../controllers/bootcamps');
 
 const router = express.Router();
+
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsByRadius)
+
 
 router.route("/").get(getBootcamps).post(postBootcamp)
 
 router.route("/:id").get(getBootcamp).put(putBootcamp).delete(deleteBootcamp)
+
 
 module.exports = router
