afad3749f15de880a30872707042f2d5b7f7a350
devinder singh <devindersingh79904@gmail.com>
2022-02-16 15:19:58 +0530

    add course route and controller added

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,7 +1,8 @@
 const asyncHandler = require("../middleware/asyncHandler")
 const Course = require("../models/Course")
+const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require("../Utils/ErrorResponse")
-const { CREATED, SUCCESS, NOT_FOUND } = require("../Utils/httpConst")
+const { CREATED, SUCCESS, NOT_FOUND, BAD_REQUEST } = require("../Utils/httpConst")
 
 
 //@desc Get courses
@@ -25,4 +26,52 @@ exports.getCourses = asyncHandler(async(req,res,next) => {
     const courses = await query;
 
     res.status(SUCCESS).json({success:true,count:courses.length,data:courses})
+})
+
+
+
+//@desc Get courses
+//@route GET /api/v1/courses/:id
+//@access Public
+
+exports.getCourse = asyncHandler(async(req,res,next) => {    
+    
+    const id = req.params.id
+    const course =await Course.findById(id).populate({
+            path : 'bootcamp',
+            select : 'name description'
+        });
+    
+        if(course == null){
+            return next(new ErrorResponse(`No course find with id ${id}`,NOT_FOUND))
+        }
+        res.status(SUCCESS).json({success:true,data:course})
+
+})
+
+
+
+//@desc ADD courses
+//@route POST /api/v1bootcamps/:bootcamId/courses
+//@access private
+
+exports.addCourse = asyncHandler(async(req,res,next) => {    
+    
+    const id = req.params.bootcampId
+
+    const bootcamp = await Bootcamp.findById(id)
+
+    if(bootcamp == null){
+        return next(new ErrorResponse( `Bootcamp with id : ${id} not found`,NOT_FOUND))
+    }
+    
+    req.body.bootcamp = id
+
+    const course = await Course.create(req.body);
+
+    if(course == null){
+        return next(new ErrorResponse( `unable to create new course`,BAD_REQUEST))
+    }
+
+    res.status(SUCCESS).json({success:true,data:course})
 })
