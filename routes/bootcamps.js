79bf55ff898716cc580bf2c0a989b3257da456e5
devinder singh <devindersingh79904@gmail.com>
2022-02-17 13:34:52 +0530

    role based authrigation added

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,7 +4,7 @@ const coursesRouter =  require('./courses')
 const { getBootcamps, getBootcamp, postBootcamp, putBootcamp, deleteBootcamp, getBootcampsByRadius, bootcampPhotoUpload } = require('../controllers/bootcamps');
 const advancedResult = require('../middleware/advanceResult');
 const Bootcamp = require('../models/Bootcamp');
-const {protect} = require('../middleware/auth');
+const {protect,authorize} = require('../middleware/auth');
 
 const router = express.Router();
 
@@ -19,7 +19,7 @@ router.route("/:id/photo").put(bootcampPhotoUpload)
 router.route("/").get(advancedResult(Bootcamp,{
     path:'courses',
     select : 'title'
-}),getBootcamps).post(postBootcamp)
+}),getBootcamps).post(protect,authorize('publisher'), postBootcamp)
 
 router.route("/:id").get(getBootcamp).put(protect,putBootcamp).delete(protect,deleteBootcamp)
 
