d4900c7a688a0aa6f5d052ba496248780444887c
devinder singh <devindersingh79904@gmail.com>
2022-02-18 17:44:39 +0530

    review router added

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const { getReviews, getReviewById, updateReview, deleteReview, addReview } = require('../controllers/reviews');
 const advancedResult = require('../middleware/advanceResult');
 const { protect ,authorize } = require('../middleware/auth');
 const Review = require('../models/Review');
 
 
+
 const router = express.Router({mergeParams:true});
 
-router.route('/').get(protect,advancedResult(Review))
+router.use(protect)
 
-
-router.route('/:id').get(protect,).put().delete()
+router.route('/').get(protect,advancedResult(Review),getReviews).post(addReview)
+router.route('/:id').get(getReviewById).put(updateReview).delete(deleteReview)
 
 module.exports = router
